5940636af928276405e855d2a8470a87903266de
植木 祐介 <uekiyusuke@ueki-no-MacBook-Pro-5.local>
2016-11-01 14:33:55 +0900

    create AngularMaterial

---
+++ angular_matelal/js/main.js
@@ -0,0 +1,5 @@
+// Angular Materialのモジュール「ngMaterial」を読み込みます
+// モジュール「ngMaterial」がないとAngular Materialは動かない
+// angular.moduleの第一引数は、「ng-app」に指定した値と同じ値を指定する
+
+var APP = angular.module("AngularMaterialStartGuide",["ngMaterial","ngAnimate"]);
